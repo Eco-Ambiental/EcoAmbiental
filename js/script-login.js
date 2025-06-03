d297ae7f58791a0043c251ab4ff1be7d0dfd23b3
MurilloFernandesCarapia <murilincarapia@gmail.com>
2025-06-02 21:05:33 -0300

    alterações feitas

--- js/script-login.js
+++ js/script-login.js
@@ -1,14 +1,15 @@
 function login(event) {
-            event.preventDefault();
-            const nome = document.getElementById('nome').value;
-            const email = document.getElementById('email').value;
-            const senha = document.getElementById('senha').value;
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
 
-            
-            if (nome && email && senha.length >= 6) {
-                alert("Bem-vindo ao Eco Ambiental, " + nome + "!");
-                window.location.href = "index.html"; 
-            } else {
-                alert("Preencha todos os campos corretamente!");
-            }
-        }
+    console.log("Dados do login:", { nome, email, senhaLength: senha.length });
+
+    if (nome && email && senha.length >= 6) {
+        alert("Bem-vindo ao Eco Ambiental, " + nome + "!");
+        window.location.href = "index.html"; 
+    } else {
+        alert("Preencha todos os campos corretamente!");
+    }
+}
